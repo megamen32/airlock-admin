c6a890149a614221da28f30c313bf737e89cb659
Cyber Teaborg <cyberteaborg@gmail.com>
2026-07-27 22:13:54 +0300

    release: airlock v0.4.1

--- frontend/src/utils/airCommands.test.ts
+++ frontend/src/utils/airCommands.test.ts
@@ -3,8 +3,8 @@ import { airlockCloneCommand, airlockInitCommand, airlockInstallCommand } from '
 
 describe('Air CLI commands', () => {
   it('installs the versioned global launcher', () => {
-    expect(airlockInstallCommand('github.com/airlockrun/agentsdk/cmd/airlock', '0.4.1-alpha.1')).toBe(
-      'go install github.com/airlockrun/agentsdk/cmd/airlock@v0.4.1-alpha.1',
+    expect(airlockInstallCommand('github.com/airlockrun/agentsdk/cmd/airlock', '0.4.1')).toBe(
+      'go install github.com/airlockrun/agentsdk/cmd/airlock@v0.4.1',
     )
   })
 
