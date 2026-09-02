0e226852cd805679c431607c3679cec874842956
roomhackerfrom100 <megamen932@gmai.com>
2026-09-02 14:03:28 +0300

    fix(cloudos): give terminal independent machine tabs

--- browser-os/lib/computer-selection.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
-
-interface ComputerSelection { computerId: string; selectComputer: (computerId: string) => void; }
-export const useComputerSelection = create<ComputerSelection>()(persist(
-  set => ({ computerId: '', selectComputer: computerId => set({ computerId }) }), { name: 'cloudos-selected-computer' },
-));
